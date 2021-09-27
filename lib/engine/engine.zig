84cd0aa56c932d6f05ca4d40995969525c3ee30d
Brian Blumberg <blum@disroot.org>
2021-09-27 12:53:39 -0700

    Prepare for engine submodule

--- lib/engine/engine.zig
+++
@@ -1,6 +0,0 @@
-pub const EntityManager = @import("entity_manager.zig").EntityManager;
-pub const SystemManager = @import("system_manager.zig").SystemManager;
-pub const Renderer = @import("renderer.zig").Renderer;
-pub const Window = @import("window.zig").Window;
-pub const Input = @import("input.zig").Input;
-pub const Clock = @import("clock.zig").Clock;
